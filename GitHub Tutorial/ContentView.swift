6be4f87931c2b8edd4982864bd27e59d263f9836
Chloe Wu <chloewu0603@gmail.com>
2024-06-21 10:32:58 -0700

    Initial Commit

---
+++ GitHub Tutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHub Tutorial
+//
+//  Created by test on 6/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitHub Tutorial/GitHub_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_TutorialApp.swift
+//  GitHub Tutorial
+//
+//  Created by test on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
